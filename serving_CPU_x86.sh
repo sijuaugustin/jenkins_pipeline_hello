26ef8832e56f87fc5d133c083b86db949503d1e0
sabari <sabari.m@attinadsoftware.com>
2019-10-17 17:18:40 +0530

    dockerhub push addedd

--- serving_CPU_x86.sh
+++ serving_CPU_x86.sh
@@ -3,5 +3,5 @@ sudo docker run -d --name $id tensorflow/serving
 echo 'copy the SavedModel to the containers model folder '
 sudo docker cp /home/$USER/$package_name $id:/models/$package_name
 echo "commiting the container that's serving the model by changing MODEL_NAME to match the model's name "
-sudo docker commit --change 'ENV MODEL_NAME ${params.package_name}' $id $package_name
+sudo docker commit --change 'ENV MODEL_NAME $package_name' $id $package_name
 sudo docker kill $id
