f1e0f9c5cc3b64126858375cdfd94738adc8fe5f
sabari <sabari.m@attinadsoftware.com>
2019-10-18 15:48:16 +0530

    sudo added

--- getModelzip.sh
+++ getModelzip.sh
@@ -2,5 +2,5 @@ echo 'creating model package folder for serving '
 sudo mkdir -p /home/$USER/$package_name/1
 sudo wget -P /home/$USER/$package_name/1 $url
 cd /home/$USER/$package_name/1
-unzip  \*.zip
-rm *.zip
+sudo unzip  \*.zip
+sudo rm *.zip
