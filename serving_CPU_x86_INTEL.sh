aa02edd0f8cbba3ccf589dc9030d986cd4a8f314
sabari <sabari.m@attinadsoftware.com>
2019-10-21 15:09:53 +0530

    zip avoid

--- serving_CPU_x86_INTEL.sh
+++ serving_CPU_x86_INTEL.sh
@@ -5,5 +5,5 @@ echo docker cp /home/$USER/$package_name $id:/models/$package_name
 sudo docker cp /home/$USER/$package_name $id:/models/$package_name
 echo "commiting the container that's serving the model by changing MODEL_NAME to match the model's name "
 echo sudo docker commit --change ENV MODEL_NAME $package_name $id $package_name
-sudo docker commit --change 'ENV MODEL_NAME $package_name' $package_name$id
+sudo docker commit --change 'ENV MODEL_NAME $package_name' $id $package_name$id
 sudo docker kill $id
