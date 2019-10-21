9cf2439538d91ecef05aebe124b707fa9627fbc1
sabari <sabari.m@attinadsoftware.com>
2019-10-21 15:04:10 +0530

    zip avoid

--- dockerhub_push.sh
+++ dockerhub_push.sh
@@ -1,4 +1,4 @@
 echo 'Pushing the model package to dockerhub '
 echo ' "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin'
-sudo docker tag $package_name aimarketplace/models:$package_name
-sudo docker push aimarketplace/models:$package_name
+sudo docker tag $package_name$id aimarketplace/models:$package_name$id
+sudo docker push aimarketplace/models:$package_name$id
