c1f09433cdaab5b1ca6741e4636c5ed495cb8e09
sabari <sabari.m@attinadsoftware.com>
2019-10-21 15:53:36 +0530

    zip avoid

--- getModelzip.sh
+++ getModelzip.sh
@@ -1,6 +1,6 @@
 echo 'creating model package folder for serving '
-sudo mkdir -p /home/$USER/$package_name/1
-sudo wget -P /home/$USER/$package_name/1 $url
-cd /home/$USER/$package_name/1
+sudo mkdir -p /home/$USER/$package_name$id/1
+sudo wget -P /home/$USER/$package_name$id/1 $url
+cd /home/$USER/$package_name$id/1
 sudo unzip  \*.zip
 sudo rm *.zip
--- serving_CPU_x86_INTEL.sh
+++ serving_CPU_x86_INTEL.sh
@@ -1,8 +1,8 @@
 echo 'Running TF serving as a daemon '
 sudo docker run -d --name $id tensorflow/serving
 echo 'copy the SavedModel to the containers model folder '
-echo docker cp /home/$USER/$package_name $id:/models/$package_name
-sudo docker cp /home/$USER/$package_name $id:/models/$package_name
+echo docker cp /home/$USER/$package_name$id $id:/models/$package_name
+sudo docker cp /home/$USER/$package_name$id $id:/models/$package_name
 echo "commiting the container that's serving the model by changing MODEL_NAME to match the model's name "
 echo sudo docker commit --change ENV MODEL_NAME $package_name $id $package_name
 sudo docker commit --change "ENV MODEL_NAME $package_name" $id $package_name$id
