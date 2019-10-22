77788e92b0d18c0b14cf92937267eaa17a0e7e08
sabari <sabari.m@attinadsoftware.com>
2019-10-22 12:01:11 +0530

    final build

--- cleanData.sh
+++ cleanData.sh
@@ -1,7 +1,7 @@
 echo 'deleteing unwanted files '
 cd /home/$USER/$p
 sudo rm -rf /home/$USER/$package_name$id
-sudo docker rmi $DOCKER_USERNAME/DOCKER_REPO:$package_name$id
+sudo docker rmi $DOCKER_USERNAME/$DOCKER_REPO:$package_name$id
 sudo docker rmi $package_name$id:latest
 
 
