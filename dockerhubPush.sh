6a917355bd93829d66988c708b62b9e70cc2750e
sijukaugustin <naithan&kin7>
2019-10-24 12:10:04 +0530

    sudo removed

--- dockerhubPush.sh
+++ dockerhubPush.sh
@@ -1,4 +1,4 @@
 echo 'Pushing the model package to dockerhub '
-echo ' "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin'
+echo ' "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin'
 docker tag $package_name$id $DOCKER_USERNAME/$DOCKER_REPO:$package_name$id
 docker push aimarketplace/models:$package_name$id
